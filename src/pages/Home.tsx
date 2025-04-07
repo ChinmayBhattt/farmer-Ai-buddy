9fc6f4d043067681781926fec7525200257b1bea
Chinmay Bhatt <123961463+ChinmayBhattt@users.noreply.github.com>
2025-04-07 13:42:06 +0530

    add more

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,7 +19,7 @@ const Home = () => {
   return (
     <div className="flex flex-col h-full bg-gray-50">
       <div className="p-4">
-        <h1 className="text-2xl font-bold mb-4">Plantix</h1>
+        <h1 className="text-2xl font-bold mb-4">FarmAI</h1>
         
         {/* Crop Selection */}
         <div className="flex space-x-4 overflow-x-auto mb-6 pb-2">
